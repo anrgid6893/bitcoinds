3ced2cbcf1f246fbd72cd01bb24debeb197ed500
anrgid6893 <dschoi@inha.edu>
2017-07-04 00:33:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-rds = requests.get("https://api.korbit.co.kr/v1/ticker")
-data = json.loads(rds.text)
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
@@ -39,54 +39,32 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "KorbitBitcoin":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+
+    rds = requests.get("https://api.korbit.co.kr/v1/ticker")
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+    data = json.loads(rds.text)
+    print("Response:")
+    print(data['last'])
+    
+    res = makeWebhookResult(data["last"])
     return res
 
 
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
+    cname = parameters.get("currency-name")
+    if cname is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + cname + "')"
 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    
+    speech = "bit-coin currency is " + data + "won."
 
     print("Response:")
     print(speech)
