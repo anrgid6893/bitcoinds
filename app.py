2e3cce642960ed33e312f263d349461cf9045717
anrgid6893 <dschoi@inha.edu>
2017-07-04 11:13:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
 
-import urllib
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
 
@@ -16,61 +22,31 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
+    if req.get("result").get("action") != "KorbitBitcoin":
+        return {}
 
-    if req.get("result").get("action") == "KorbitBitcoin":
-        parameters = req.get("result").get("parameters")
-        currency_symbol = parameters.get("currency-name")
+    baseurl = "https://blockchain.info/de/ticker"
+    result = urlopen(baseurl).read()
+    data = json.loads(result)
+    res = data["USD"]["last"]
+    r = makeWebhookResult(res)
 
-        baseurl = "https://blockchain.info/de/ticker"
-        result = urllib.urlopen(baseurl).read()
-        data = json.loads(result)
-
-        query = data.get(currency_symbol)
-
-        speech = "Bitcoin exchange rates:"
-
-        slack_message = {
-            "text": speech,
-            "attachments": [
-                {
-                    "title": "Bitcoin",
-                    "title_link": "https://markets.blockchain.info",
-                    "color": "#36a64f",
-
-                    "fields": [
-                        {
-                            "title": "Last",
-                            "value": str(query.get('last')) + " " + query.get('symbol'),
-                            "short": "false"
-                        },
-                        {
-                            "title": "Buy",
-                            "value": str(query.get('buy')) + " " + query.get('symbol'),
-                            "short": "false"
-                        },
-                        {
-                            "title": "Sell",
-                            "value": str(query.get('sell')) + " " + query.get('symbol'),
-                            "short": "false"
-                        }
-                    ],
-
-                    "thumb_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/440px-Bitcoin.svg.png"
-                }
-            ]
-        }
-
-        res = {
-            "speech": speech,
-            "displayText": speech,
-            "data": {"slack": slack_message},
-            "source": "apiai-bitcoin-webhook"
-        }
-
-    res = json.dumps(res, indent=4)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
     return r
+
+
+def makeWebhookResult(data):
+    speech = "Now bit coin currency is " + data + "!"
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        # "source": "apiai-weather-webhook-sample"
+    }
 
 
 if __name__ == '__main__':
